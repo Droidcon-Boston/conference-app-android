bc456926a47bf37a2bdc2bcb7a639649e9af2119
kimj <jinn.kim@gmail.com>
2017-03-30 01:19:40 -0400

    more adapter work. made an item layout, deleted test folder

--- Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/views/agenda/ScheduleAdapter.java
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/views/agenda/ScheduleAdapter.java
@@ -1,12 +1,10 @@
 package com.mentalmachines.droidcon_boston.views.agenda;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,11 +14,14 @@ import com.mentalmachines.droidcon_boston.data.model.DroidconSchedule;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by jinn on 3/27/17.
  */
 
-public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHolder> {
+public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ScheduleItemViewHolder> {
     private List<DroidconSchedule> schedule;
     private Context mContext;
 
@@ -60,30 +61,36 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mTextView.setText(schedule.get(position).title.toString());
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ScheduleItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(
                 R.layout.schedule_card_view, parent, false);
-        ViewHolder holder = new ViewHolder(view);
+        ScheduleItemViewHolder holder = new ScheduleItemViewHolder(view);
         return holder;
     }
 
+    @Override
+    public void onBindViewHolder(ScheduleItemViewHolder holder, int position) {
+        holder.titleText.setText(schedule.get(position).title.toString());
+        holder.timeText.setText(schedule.get(position).title.toString());
+        holder.locationText.setText(schedule.get(position).title.toString());
+        holder.speakerNameText.setText(schedule.get(position).title.toString());
+    }
+
     public static class ScheduleItemViewHolder extends RecyclerView.ViewHolder {
-        CardView cv;
-        TextView scheduleTitle;
-        TextView personAge;
-        ImageView personPhoto;
+        @BindView(R.id.title_text)
+        TextView titleText;
+        @BindView(R.id.time_text)
+        TextView timeText;
+        @BindView(R.id.location_text)
+        TextView locationText;
+        @BindView(R.id.speaker_name_text)
+        TextView speakerNameText;
 
         ScheduleItemViewHolder(View itemView) {
             super(itemView);
-/*            cv = (CardView)itemView.findViewById(R.id.);
-            scheduleTitle = (TextView)itemView.findViewById(R.id.schedule_item_title);
-            personAge = (TextView)itemView.findViewById(R.id.schedule_item_time);
-             = (ImageView)itemView.findViewById(R.id.schedulew_item_location);*/
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(v -> {
+            });
         }
     }
 }
