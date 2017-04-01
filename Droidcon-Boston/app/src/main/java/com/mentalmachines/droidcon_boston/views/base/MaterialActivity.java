234159c9a08b9b3c0d8d0b153047b34476512ebc
emezias <emezias@gmail.com>
2017-04-01 19:08:33 -0400

    Changing up the MainActivity layout to have a navigation drawer

--- Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/views/base/MaterialActivity.java
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/views/base/MaterialActivity.java
@@ -1,17 +1,8 @@
 package com.mentalmachines.droidcon_boston.views.base;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.mentalmachines.droidcon_boston.R;
 
@@ -23,9 +14,7 @@ import butterknife.ButterKnife;
  */
 
 public abstract class MaterialActivity extends BaseActivity {
-    // @BindView(R.id.fab) FloatingActionButton fab;
-    // @BindView(R.id.recycler) RecyclerView recycler;
-    @BindView(R.id.bottom_navigation) BottomNavigationView bottomNavigationView;
+
     @BindView(R.id.toolbar) Toolbar toolbar;
     Snackbar snackbar;
 
@@ -38,8 +27,7 @@ public abstract class MaterialActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
-        // getActionBar().setDisplayHomeAsUpEnabled(true);
-        // getActionBar().setHomeButtonEnabled(true);
+
     }
 
     @Override
