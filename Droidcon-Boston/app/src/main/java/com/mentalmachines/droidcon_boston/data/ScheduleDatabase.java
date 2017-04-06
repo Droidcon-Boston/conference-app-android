c820da8725fa783b547192b1851f84663f392016
emezias <emezias@gmail.com>
2017-04-06 00:24:47 -0400

    finishing up FAQ

--- Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/data/ScheduleDatabase.java
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/data/ScheduleDatabase.java
@@ -23,7 +23,7 @@ public class ScheduleDatabase extends SQLiteAssetHelper {
 
     public static final String TAG = "ScheduleDatabase";
     private static SQLiteDatabase sDB;
-    private static final String DATABASE_NAME = "droidcon_boston_v01.db";
+    private static final String DATABASE_NAME = "dc2017boston.db";
     private static final int DATABASE_VERSION = 1;
     //columns, database constants
     public static final String TABLE = "speaker";
