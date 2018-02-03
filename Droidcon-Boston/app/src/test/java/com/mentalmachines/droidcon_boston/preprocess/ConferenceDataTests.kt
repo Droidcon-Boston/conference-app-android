35694f0ad18d0c081dccabf2c6d271b4ecb63b01
Ken Yee <kyee@kayak.com>
2018-02-03 13:03:15 -0500

    Add test parsing of SlidesUp test data and a denormalizer script to massage data before it's imported into Firebase

---
+++ Droidcon-Boston/app/src/test/java/com/mentalmachines/droidcon_boston/preprocess/ConferenceDataTests.kt
@@ -0,0 +1,30 @@
+package com.mentalmachines.droidcon_boston.preprocess
+
+import com.mentalmachines.droidcon_boston.preprocess.ConferenceDataUtils.Companion.denormalizeConferenceData
+import com.mentalmachines.droidcon_boston.preprocess.ConferenceDataUtils.Companion.getMoshiInstance
+import okio.Okio
+import org.junit.Test
+
+class ConferenceDataTests {
+
+    @Test
+    fun testReadConfData() {
+        val datastream = this.javaClass.classLoader.getResourceAsStream("sample-conference-export.json")
+
+        val moshi = getMoshiInstance()
+        val jsonAdapter = moshi.adapter(ConferenceDataModel::class.java)
+        val confData = jsonAdapter.fromJson(Okio.buffer(Okio.source(datastream)))
+
+        assert(confData?.events?.size ?: 0 > 0)
+        assert(confData?.rooms?.size ?: 0 > 0)
+        assert(confData?.sections?.size ?: 0 > 0)
+        assert(confData?.speakers?.size ?: 0 > 0)
+        assert(confData?.tracks?.size ?: 0 > 0)
+
+        denormalizeConferenceData(confData)
+
+        confData?.events?.forEach {
+            assert(it.value.speakerNames?.size ?: 0 > 0)
+        }
+    }
+}
---
+++ Droidcon-Boston/app/src/test/java/com/mentalmachines/droidcon_boston/preprocess/ConferenceDataUtils.kt
@@ -0,0 +1,48 @@
+package com.mentalmachines.droidcon_boston.preprocess
+
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Rfc3339DateJsonAdapter
+import java.util.*
+
+class ConferenceDataUtils {
+    companion object {
+
+        fun getMoshiInstance(): Moshi {
+            val moshi = Moshi.Builder()
+                    .add(KotlinJsonAdapterFactory())
+                    .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
+                    .build()
+            return moshi
+        }
+
+        fun denormalizeConferenceData(confData: ConferenceDataModel?) {
+            confData?.events?.forEach {
+                // denormalize speakers
+                val speakerNames = HashMap<String, Boolean>()
+                it.value.speakerIds?.forEach {
+                    confData.speakers.get(it.key)?.let {
+                        speakerNames.put(it.name, true)
+                    }
+                }
+                if (speakerNames.size > 0) {
+                    it.value.speakerNames = speakerNames
+                }
+                // denormalize rooms
+                val roomNames = HashMap<String, Boolean>()
+                it.value.roomIds?.forEach {
+                    confData.rooms.get(it.key)?.let {
+                        roomNames.put(it.name, true)
+                    }
+                }
+                if (roomNames.size > 0) {
+                    it.value.roomNames = roomNames
+                }
+                // look up track name
+                it.value.trackId?.apply {
+                    it.value.trackName = confData.tracks.get(it.value.trackId!!)?.name
+                }
+            }
+        }
+    }
+}
