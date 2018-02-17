c1083fae23d79238cfadc9e65a6eedd3614e0e65
emezias <emezias@gmail.com>
2018-02-16 23:17:30 -0500

    Converted the Kotlin data classes to a class format that is compatible with Firebase queries. Set all of the speaker photos into res. Starting on Firebase queries with sample data while we wait for the 2018 agenda to be set.

--- Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/data/ConferenceData.java
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/data/ConferenceData.java
@@ -2,12 +2,17 @@ package com.mentalmachines.droidcon_boston.data;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import com.mentalmachines.droidcon_boston.R;
 import com.mentalmachines.droidcon_boston.data.ScheduleDatabase.ScheduleRow;
+import com.mentalmachines.droidcon_boston.firebase.ScheduleUpdateUtils;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by emezias on 2/10/18 to read static data and create a local cache in memory
@@ -20,7 +25,25 @@ public class ConferenceData {
     public static final String TAG = ConferenceData.class.getSimpleName();
     private static SimpleDateFormat sTimeFormat = new SimpleDateFormat("h:mm a", Locale.US);
     private static SimpleDateFormat sDateFormat = new SimpleDateFormat("M/d/yyyy", Locale.US);
+    private static SimpleDateFormat sMakeDateObject = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
     public static ConferenceDataModel sConfData;
+
+    public static class EventData {
+        public String name;
+        String description;
+        String duration;
+        Boolean isGeneralEvent;
+        Boolean isPublished;
+        Date startTime;
+        Map<String, Boolean> roomIds;
+        Map<String, Boolean> speakerIds;
+        String trackId;
+        Map<String, Boolean> roomNames;
+        Map<String, Boolean> speakerNames;
+        String trackName;
+        Long updatedAt;
+        String updatedBy;
+    }
 
     /**
      * Context is needed to get the json file from assets
@@ -32,6 +55,7 @@ public class ConferenceData {
         ScheduleRow listItem;
         if (sConfData == null) {
             sConfData = ConferenceDataUtils.Companion.parseData(ctx);
+            sConfData = ScheduleUpdateUtils.INSTANCE.checkForChanges(sConfData, 0l);
         }
         ArrayList<ScheduleRow> eventData = new ArrayList<>();
         for (EventModel event: sConfData.getEvents().values()) {
@@ -59,10 +83,15 @@ public class ConferenceData {
         }
         listItem.talkTitle = event.getName();
         if (event.getStartTime() != null) {
-            listItem.date = event.getStartTime();
-            listItem.time = sTimeFormat.format(listItem.date);
-            listItem.dateString = sDateFormat.format(listItem.date);
-            //Log.d(TAG, "date string for compare " + listItem.dateString);
+            try {
+                listItem.date = sMakeDateObject.parse(event.getStartTime());
+                listItem.time = sTimeFormat.format(listItem.date);
+                listItem.dateString = sDateFormat.format(listItem.date);
+                //Log.d(TAG, "date string for compare " + listItem.dateString);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                Log.e(TAG, "date error: " + e.getMessage());
+            }
         }
         if (event.getRoomIds() != null) {
             listItem.room = (String) event.getRoomNames().keySet().toArray()[0];
@@ -98,44 +127,70 @@ public class ConferenceData {
         return detailData;
     }
 
+    /**
+     * This switch is a way to use data from the res directory for speaker's photos
+     * It may be ugly but it will help us to keep the WiFi at the conference responsive
+     * @param speakerFirstName
+     * @return - resource id for speaker photo
+     */
     static int uglySwitch(String speakerFirstName) {
         switch (speakerFirstName) {
             case ADRIAN:
                 return R.drawable.adrian_catalan;
-            case JULIAN:
-                return R.drawable.julian_krzeminski;
+            case ADNAN:
+                return R.drawable.adnan_a_m;
             case ALICE:
                 return R.drawable.alice_yuan;
-            case KELLY:
-                return R.drawable.kelly_schuster;
             case ANA:
                 return R.drawable.ana_baotic;
-            case LISA:
-                return R.drawable.lisa_wray;
+            case CARMEN:
+                return R.drawable.carmen_gonzalez;
+            case CLIVE:
+                return R.drawable.clive_lee;
             case DANNY:
                 return R.drawable.danny_preussler;
+            case EFFIE:
+                return R.drawable.effie_barak;
+            case ELIZA:
+                return R.drawable.eliza_camberogiannis;
+            case ERIC:
+                return R.drawable.eric_maxwell;
+            case GARIMA:
+                return R.drawable.garima_jain;
+            case JEB:
+                return R.drawable.jeb_ware;
+            case JOE:
+                return R.drawable.joe_birch;
+            case JULIAN:
+                return R.drawable.julian_krzeminski;
+            case KAAN:
+                return R.drawable.kaan_mamikoglu;
+            case KELLY:
+                return R.drawable.kelly_schuster;
+            case LISA:
+                return R.drawable.lisa_wray;
             case LORA:
                 return R.drawable.lora_kulm;
             case DOUG:
                 return R.drawable.doug_stevenson;
             case MANNY:
                 return R.drawable.manuel_vicente_vivo;
-            case ELIZA:
-                return R.drawable.eliza_camberogiannis;
             case MERCEDES:
                 return R.drawable.mercedes_wyss;
-            case GARIMA:
-                return R.drawable.garima_jain;
+            case MICHAEL:
+                return R.drawable.michael_evans;
             case NATE:
                 return R.drawable.nate_ebel;
-            case JEB:
-                return R.drawable.jeb_ware;
-            case NICK:
+            case NICHOLAS:
                 return R.drawable.nicholas_dipatri;
-            case JOE:
-                return R.drawable.joe_birch;
+            case PANAYOTIS:
+                return R.drawable.panayotis_tzinis;
             case SAM:
                 return R.drawable.sam_edwards;
+            case SHOHEI:
+                return R.drawable.shohei_kawano;
+            case ZAC:
+                return R.drawable.zac_sweers;
         }
         return  - 1;
     }
@@ -146,22 +201,32 @@ public class ConferenceData {
     public static final String GITHUB  = "github";
     public static final String LINKD = "linkedIn";
     //constants for the speaker images in res
+    public static final String ADNAN = "adnan";
     public static final String ADRIAN = "adrian";
-    public static final String JULIAN = "julian";
     public static final String ALICE = "alice";
-    public static final String KELLY = "kelly";
     public static final String ANA = "ana";
-    public static final String LISA = "lisa";
+    public static final String CARMEN = "carmen";
+    public static final String CLIVE = "clive";
     public static final String DANNY = "danny";
-    public static final String LORA = "lora";
     public static final String DOUG = "doug";
-    public static final String MANNY = "manuel";
+    public static final String EFFIE = "effie";
     public static final String ELIZA = "eliza";
-    public static final String MERCEDES = "mercedes";
+    public static final String ERIC = "eric";
     public static final String GARIMA = "garima";
-    public static final String NATE = "nate";
     public static final String JEB = "jeb";
-    public static final String NICK = "nicholas";
     public static final String JOE = "joe";
+    public static final String JULIAN = "julian";
+    public static final String KAAN = "kaan";
+    public static final String KELLY = "kelly";
+    public static final String LISA = "lisa";
+    public static final String LORA = "lora";
+    public static final String MANNY = "manuel";
+    public static final String MERCEDES = "mercedes";
+    public static final String MICHAEL = "michael";
+    public static final String NATE = "nate";
+    public static final String NICHOLAS = "nicholas";
+    public static final String PANAYOTIS = "panayotis";
     public static final String SAM = "sam";
+    public static final String SHOHEI = "shohei";
+    public static final String ZAC = "zac";
 }
---
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/firebase/ScheduleUpdateUtils.kt
@@ -0,0 +1,51 @@
+package com.mentalmachines.droidcon_boston.firebase
+
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.mentalmachines.droidcon_boston.data.ConferenceDataModel
+import com.mentalmachines.droidcon_boston.data.EventModel
+
+/**
+ * Created by emezias on 2/13/18.
+ */
+
+object ScheduleUpdateUtils {
+    val TAG = ScheduleUpdateUtils::class.java.simpleName
+    val EVENTS = "events"
+    val ROOMS = "rooms"
+    val SECTIONS = "sections"
+    val SPEAKERS = "speakers"
+    val TRAX = "tracks"
+
+    fun checkForChanges(staticData: ConferenceDataModel, timestamp: Long): ConferenceDataModel {
+        val root = FirebaseDatabase.getInstance().getReference("conferenceData")
+        // Read from the database
+        val getScheduleData = root.child(EVENTS)//.startAt("1516821464815");
+        val listener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.childrenCount == 0L) {
+                    Log.d(TAG, "cancelled " + "no children returned")
+                    //return;
+                }
+                Log.d(TAG, "got data " + dataSnapshot.childrenCount)
+                val eventsfound: ArrayList<EventModel> = ArrayList()
+                dataSnapshot.children.mapNotNullTo(eventsfound) { it.getValue<EventModel>(EventModel::class.java) }
+
+                for (eventItem in eventsfound) {
+                    //val note = noteDataSnapshot.getValue(ConferenceData.EventData::class.java)
+                    Log.d(TAG, "got event " + eventItem!!.name)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e(TAG, "cancelled " + databaseError.message)
+            }
+        }
+        getScheduleData.addListenerForSingleValueEvent(listener)
+        Log.d(TAG, "set listener")
+        return staticData
+    }
+}
