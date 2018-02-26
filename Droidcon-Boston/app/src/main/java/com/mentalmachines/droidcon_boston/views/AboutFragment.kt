cee973974951ce75cafdf92074f94a4a28641d51
Nishant Srivastava <nisrulz@gmail.com>
2018-02-26 11:07:35 +0530

    added: AboutFragment and wired it to the nav drawer link

---
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/views/AboutFragment.kt
@@ -0,0 +1,49 @@
+package com.mentalmachines.droidcon_boston.views
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import butterknife.ButterKnife
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.mentalmachines.droidcon_boston.R
+import com.mentalmachines.droidcon_boston.firebase.FirebaseHelper
+import kotlinx.android.synthetic.main.about_fragment.tv_about_description
+
+class AboutFragment : Fragment() {
+
+    private val firebaseHelper = FirebaseHelper.instance
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val rootView = inflater.inflate(R.layout.about_fragment, container, false)
+        ButterKnife.bind(this, rootView)
+        return rootView
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fetchDataFromFirebase()
+    }
+
+
+    private fun fetchDataFromFirebase() {
+        firebaseHelper.aboutDatabase.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                tv_about_description.text = dataSnapshot.getValue(String::class.java)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e(javaClass.canonicalName, "onCancelled", databaseError.toException())
+            }
+        })
+
+    }
+}
