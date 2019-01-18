e0737371c95df21e635b693249ff37c41ac50595
ccorrado <ccorrads@gmail.com>
2019-01-17 21:07:09 -0500

    Use the Firebase UID for user ids (+5 squashed commits) Squashed commits: [1b4b95c] Working prototype with Firebase Auth + Database. Storing user as a child under attendees in database. Adding a few tests. [7630760] Adding a sample Eventbrite user response object model. Added a few validation tests... and some sample data ;) [c4dcbe6] Add in circle config [caef7ba] PR Feedback [3db8344] Create User object data class. Add User Firebase Database table. Stub out the data change listener inside of the auth controller.

--- Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/firebase/AuthController.kt
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/firebase/AuthController.kt
@@ -4,41 +4,58 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.annotation.DrawableRes
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.mentalmachines.droidcon_boston.data.FirebaseDatabase
+import com.mentalmachines.droidcon_boston.data.UserAgendaRepo
 
-class AuthController(@DrawableRes private val loginScreenAppIcon: Int) {
+class AuthController {
+
     private var user: FirebaseUser? = null
 
     val isLoggedIn: Boolean
         get() = (user != null)
 
-    fun login(activity: AppCompatActivity, resultCode: Int) {
+    fun login(
+        activity: AppCompatActivity, resultCode: Int,
+        @DrawableRes loginScreenAppIcon: Int
+    ) {
 
         val providers = arrayListOf(
-            AuthUI.IdpConfig.GoogleBuilder().build())
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
 
         activity.startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(providers)
                 .setLogo(loginScreenAppIcon)
-                .build(),
-            resultCode)
+                .build(), resultCode
+        )
     }
 
     /***
      * Returns an error message if there was a login error
      * or null if successful
      */
-    fun handleLoginResult(resultCode: Int, data: Intent?): String? {
+    fun handleLoginResult(context: Context, resultCode: Int, data: Intent?): String? {
         val response = IdpResponse.fromResultIntent(data)
 
         return if (resultCode == Activity.RESULT_OK) {
             user = FirebaseAuth.getInstance().currentUser
+            user?.let { user ->
+                FirebaseHelper.instance.userDatabase.child(user.uid)
+                    .setValue(
+                        createLocalUser(
+                            UserAgendaRepo.getInstance(context).savedSessionIds,
+                            user
+                        )
+                    )
+            }
             null
         } else {
             response?.error?.message
@@ -46,8 +63,24 @@ class AuthController(@DrawableRes private val loginScreenAppIcon: Int) {
     }
 
     fun logout(context: Context) {
-        AuthUI.getInstance()
-            .signOut(context)
+        AuthUI.getInstance().signOut(context)
+        user?.email?.let { email ->
+            FirebaseHelper.instance.userDatabase.child(email).setValue(
+                null
+            )
+        }
         user = null
+    }
+
+    @VisibleForTesting
+    fun createLocalUser(sessionIds: Set<String>, user: FirebaseUser): FirebaseDatabase.User {
+        return FirebaseDatabase.User(
+            user.uid,
+            user.email,
+            user.photoUrl?.toString(),
+            user.displayName,
+            "",
+            sessionIds.toList()
+        )
     }
 }
