6d16a23624ee796e2b647e623b227bae8169eae9
adammc331 <amcneilly331@gmail.com>
2019-01-27 18:39:14 -0500

    Refactored test for #138 to put some variables outside the test methods.

--- Droidcon-Boston/app/src/test/java/com/mentalmachines/droidcon_boston/data/FirebaseDatabaseTest.kt
+++ Droidcon-Boston/app/src/test/java/com/mentalmachines/droidcon_boston/data/FirebaseDatabaseTest.kt
@@ -5,35 +5,45 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class FirebaseDatabaseTest {
+    private val notifTime = 10L
+    private val primarySpeakerName = "Primary Speaker"
+    private val startTime = "2018-03-26T15:00:00.000Z"
+    private val startTimeFormatted = "11:00 am"
+    private val dateFormatted = "03/26/2018"
+    private val talkTitle = "Talk Name"
+    private val speakerOrg = "Speaker Org"
+    private val speakerPicture = "Picture URL"
+    private val speakerNames = hashMapOf(primarySpeakerName to true, "Second Speaker" to true)
+    private val speakerNamesToPhotos =
+        hashMapOf(primarySpeakerName to speakerPicture, "Second" to "Second Photo")
+    private val speakerNameToOrg =
+        hashMapOf(primarySpeakerName to speakerOrg, "Second Speaker" to "Second Company")
+    private val roomNames = hashMapOf("Talk Room" to true)
+    private val speakerIds = hashMapOf("SpeakerOne" to true, "SpeakerTwo" to true)
+    private val roomIds = hashMapOf("RoomOne" to true)
+    private val description = "Talk Description"
+    private val photo = hashMapOf("Photo" to "Photo")
+    private val endTime = "2018-03-26T15:45:00.000Z"
+    private val endTimeFormatted = "11:45 am"
+    private val trackSortOrder = 0
+    private val scheduleId = "ScheduleId"
+    private val speakerBio = "Speaker Bio"
+    private val facebookProfile = "Facebook Profile"
+    private val linkedinProfile = "LinkedIn Profile"
+    private val twitterProfile = "Twitter Profile"
+    private val socialProfiles = hashMapOf(
+        "facebook" to facebookProfile, "linkedIn" to
+                linkedinProfile,
+        "twitter" to twitterProfile
+    )
 
     @Test
     fun scheduleEventToScheduleRow() {
-        val notifTime = 10L
-        val primarySpeakerName = "Primary Speaker"
-        val startTime = "2018-03-26T15:00:00.000Z"
-        val startTimeFormatted = "11:00 am"
-        val dateFormatted = "03/26/2018"
-        val name = "Talk Name"
-        val speakerNames = hashMapOf("First Speaker" to true, "Second Speaker" to true)
-        val speakerNamesToPhotos = hashMapOf("First Speaker" to "First Photo", "Second Speaker" to 
-                "Second Photo")
-        val speakerNameToOrg = hashMapOf("First Speaker" to "First Company", "Second Speaker" to 
-                "Second Company")
-        val roomNames = hashMapOf("Talk Room" to true)
-        val speakerIds = hashMapOf("SpeakerOne" to true, "SpeakerTwo" to true)
-        val roomIds = hashMapOf("RoomOne" to true)
-        val description = "Talk Description"
-        val photo = hashMapOf("Photo" to "Photo")
-        val endTime = "2018-03-26T15:45:00.000Z"
-        val endTimeFormatted = "11:45 am"
-        val trackSortOrder = 0
-        val scheduleId = "ScheduleId"
-
         val testEvent = FirebaseDatabase.ScheduleEvent(
             notifTime,
             primarySpeakerName,
-            startTime, 
-            name,
+            startTime,
+            talkTitle,
             speakerNames,
             speakerNamesToPhotos,
             speakerNameToOrg,
@@ -51,7 +61,7 @@ class FirebaseDatabaseTest {
         assertEquals(primarySpeakerName, testRow.primarySpeakerName)
         assertEquals(scheduleId, testRow.id)
         assertEquals(startTimeFormatted, testRow.startTime)
-        assertEquals(name, testRow.talkTitle)
+        assertEquals(talkTitle, testRow.talkTitle)
         assertEquals(speakerNames.count(), testRow.speakerCount)
         assertEquals(description, testRow.talkDescription)
         assertEquals(speakerNames.keys.toList(), testRow.speakerNames)
@@ -67,33 +77,21 @@ class FirebaseDatabaseTest {
 
     @Test
     fun eventSpeakerToScheduleDetail() {
-        val pictureUrl = "Picture URL"
-        val facebookProfile = "Facebook Profile"
-        val linkedinProfile = "LinkedIn Profile"
-        val twitterProfile = "Twitter Profile"
-        val socialProfiles = hashMapOf("facebook" to facebookProfile, "linkedIn" to
-                linkedinProfile,
-            "twitter" to twitterProfile)
-        val bio = "Speaker Bio"
-        val title = "Talk Title"
-        val org = "Speaker Org"
-        val name = "Speaker Name"
-
         val testEventSpeaker = FirebaseDatabase.EventSpeaker(
-            pictureUrl,
+            speakerPicture,
             socialProfiles,
-            bio,
-            title,
-            org,
-            name
+            speakerBio,
+            talkTitle,
+            speakerOrg,
+            primarySpeakerName
         )
 
-        val scheduleRow = Schedule.ScheduleRow(id = "ScheduleId")
+        val scheduleRow = Schedule.ScheduleRow()
         val scheduleDetail = testEventSpeaker.toScheduleDetail(scheduleRow)
         assertEquals(scheduleRow.id, scheduleDetail.id)
         assertEquals(facebookProfile, scheduleDetail.facebook)
         assertEquals(linkedinProfile, scheduleDetail.linkedIn)
         assertEquals(twitterProfile, scheduleDetail.twitter)
-        assertEquals(bio, scheduleDetail.speakerBio)
+        assertEquals(speakerBio, scheduleDetail.speakerBio)
     }
 }
