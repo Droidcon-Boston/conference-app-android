1c9431b803bacaa1b4880eaa78e89dad1bf92231
adammc331 <amcneilly331@gmail.com>
2019-02-02 10:42:58 -0500

    Optimize import.

--- Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/views/detail/AgendaDetailFragment.kt
+++ Droidcon-Boston/app/src/main/java/com/mentalmachines/droidcon_boston/views/detail/AgendaDetailFragment.kt
@@ -1,6 +1,5 @@
 package com.mentalmachines.droidcon_boston.views.detail
 
-import android.content.Context
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
