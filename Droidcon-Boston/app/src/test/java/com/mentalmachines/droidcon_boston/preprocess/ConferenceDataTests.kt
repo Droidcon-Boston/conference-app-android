1ebdd7fcb2ddcb03f8ab6005d418ae5c94312e7f
Ken Yee <kyee@kayak.com>
2019-03-02 18:25:18 -0500

    add test for writing to the firebase db

--- Droidcon-Boston/app/src/test/java/com/mentalmachines/droidcon_boston/preprocess/ConferenceDataTests.kt
+++ Droidcon-Boston/app/src/test/java/com/mentalmachines/droidcon_boston/preprocess/ConferenceDataTests.kt
@@ -1,5 +1,9 @@
 package com.mentalmachines.droidcon_boston.preprocess
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.database.FirebaseDatabase
 import com.mentalmachines.droidcon_boston.preprocess.ConferenceDataUtils.Companion.denormalizeConferenceData
 import com.mentalmachines.droidcon_boston.preprocess.ConferenceDataUtils.Companion.getMoshiInstance
 import okhttp3.OkHttpClient
@@ -22,8 +26,32 @@ class ConferenceDataTests {
 
     @Test
     fun testDownloadConfData() {
+        val confExportJson = getSlidesupDataFromAPI(SLIDESUP_API_BASEURL)
+
+        val dataStream = confExportJson.byteInputStream()
+
+        verifyConfData(dataStream)
+    }
+
+    @Test
+    fun testWriteFirebase() {
+        val classLoader = this.javaClass.classLoader!!
+        val serviceAccount = classLoader.getResourceAsStream("droidcon-bos-firebase-adminsdk-apses-5a4c189cb9.json")
+
+        val options = FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl("https://droidcon-bos-2019.firebaseio.com/")
+            .build()
+        FirebaseApp.initializeApp(options)
+
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("chat")
+
+        ref.setValueAsync(2.0)
+    }
+
+    private fun getSlidesupDataFromAPI(baseUrl: String): String {
         val client = OkHttpClient()
-        val baseUrl = "https://slidesup-test.firebaseio.com/confs/detail/boston-conference-2018"
         val sections = getUrlRawData(client, "$baseUrl/sections.json?print=pretty")
         val tracks = getUrlRawData(client, "$baseUrl/tracks.json?print=pretty")
         val events = getUrlRawData(client, "$baseUrl/events.json?print=pretty")
@@ -35,10 +63,7 @@ class ConferenceDataTests {
                 "  \"events\" : $events,\n" +
                 "  \"speakers\" : $speakers,\n" +
                 "  \"rooms\" : $rooms\n}"
-
-        val dataStream = confExportJson.byteInputStream()
-
-        verifyConfData(dataStream)
+        return confExportJson
     }
 
     @Throws(IOException::class)
@@ -50,7 +75,7 @@ class ConferenceDataTests {
         okHttpClient.newCall(request).execute().use { response -> return response.body()?.string() }
     }
 
-    private fun verifyConfData(dataStream: InputStream?) {
+    private fun verifyConfData(dataStream: InputStream) {
         val moshi = getMoshiInstance()
         val jsonAdapter = moshi.adapter(ConferenceDataModel::class.java)
         val confData = jsonAdapter.fromJson(Okio.buffer(Okio.source(dataStream)))
@@ -66,5 +91,9 @@ class ConferenceDataTests {
         confData?.events?.forEach {
             assert(it.value.name.isNotBlank())
         }
+    }
+
+    companion object {
+        private const val SLIDESUP_API_BASEURL = "https://slidesup-test.firebaseio.com/confs/detail/boston-conference-2018"
     }
 }
